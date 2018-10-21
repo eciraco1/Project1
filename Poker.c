7f594310aa080f1de674196a8d8d2ab47251cfa3
eciraco1 <43480602+eciraco1@users.noreply.github.com>
2018-10-20 20:28:57 -0400

    Add files via upload

--- Poker.c
+++ Poker.c
@@ -1,7 +1,5 @@
-#include <stdio.h>
-typedef int bool;
-#define TRUE 1
-#define FALSE 0
+#include "poker.h"
+
 
 
 
@@ -13,61 +11,67 @@ typedef struct Cards{
 Card hand[7];
 
 
-struct Player{
+typedef struct Players{
   Card hand[7]; //makes an array of cards, check if it works
+  int count[15]; //gives the amount of times that a certain card value is seen in the hand
   int handValue;
-} p1, p2 ,p3;
-
-
-
-int getHandValue(Card h[]);
-int highest(Card h[]);
-void sortCards(Card * h[]);
-bool checkRFlush(Card h[]);
-bool isRoyalty(Card a);
-bool checkFlush(Card h[]);
-bool checkSFlush(Card h[]);
-int numOfPair(Card h[]);
-bool checkStraight(Card h[]);
-bool checkFull(Card h[]);
-bool checkFour(Card h[]);
-bool checkT(Card h[]);
-void makePlayersHands(struct Player p1, struct Player p2, struct Player p3, FILE *fp);
-Card makeCard(char num, char suite);
-
-
-
-
-
-
-
+}Player;
+Player p1, p2 ,p3;
 
 
 
 int main(){
   char buff[255];
-  FILE *fp= fopen("readin.txt", "r"); //check syntax
-  //while(fgets(buff, sizeof(buff), fp)){
-    makePlayersHands(p1, p2, p3, fp);
-    p1.handValue= getHandValue(p1.hand);
-    p2.handValue= getHandValue(p2.hand);
-    p3.handValue= getHandValue(p3.hand);
- 
+  FILE *fp; // file to read
+  FILE *ap; //used for while loop
+  FILE *pp; //file to print to
+  fp= fopen("sampleinput.txt", "r");
+  pp= fopen("finalPrint.txt", "w");
+  ap= fopen("sampleinput.txt", "r");
+  while(fgets(buff, 255, ap)!=NULL){
+    makePlayersHands(&p1, &p2, &p3, fp);
+
+
+    //---------------------
+    sortCards((p1.hand));
+    sortCards((p2.hand));
+    sortCards((p3.hand));
+
+    makeCount(&p1);
+    makeCount(&p2);
+    makeCount(&p3);
+    //------------------------
+
+    p1.handValue= getHandValue(p1);
+    int one= getHandValue(p1);
+
+    p2.handValue= getHandValue(p2);
+    int two= getHandValue(p2);
+
+    p3.handValue= getHandValue(p3);
+    int three= getHandValue(p3);
+
+
+
+
 //----
-  if (p1.handValue > p3.handValue){
-        if(p1.handValue > p2.handValue){
-          printf("Player 1 wins \n");
-        }else{
-          printf("Player 3 wins \n");
-        }
+  if (one > two){
+      if(one > three){
+        fprintf(pp, "Player 1 wins \n");
+
+      }else{
+        fprintf(pp, "Player 3 wins \n");
+      }
 //-----
-  }else if (p2.handValue > p3.handValue){
-      printf("Player 2 wins \n");
+}else if (two > three){
+      fprintf(pp, "Player 2 wins \n");
 //------
-  }else
-      printf("Player 3 wins \n");
-//}
+  }else{
+      fprintf(pp, "Player 3 wins \n");
+    }
+  }
   fclose(fp);
+  fclose(pp);
 }
 
 
@@ -76,7 +80,6 @@ int main(){
 Card makeCard(char num, char suite){
   Card ret;
   int number;
-  char sui=sui;
   if(num=='T'){
       number=10;
     }else if(num=='J'){
@@ -96,37 +99,60 @@ Card makeCard(char num, char suite){
 }
 //------------------------------------------------------------------------------------------------------------
 //Calculates hand value, whoever has the highest hand value wins
-int getHandValue(Card h[]){
+int getHandValue(Player p){
 
-  if(checkRFlush(h) )
-    return 1000+highest(h);
+  if(checkRFlush(p.hand) ){
+    //printf("Rflush\n");
+    return 1000+highest(p.hand);
+  }
 
-  if(checkSFlush( h) )
-    return 900+highest(h);
+  if(checkSFlush(p.hand) ){
+    //printf("Sflush\n");
+    return 900+highest(p.hand);
+  }
 
-  if(checkFour( h) )
-    return 800+highest(h);
+  if(checkFour( p.count) ){
+    //printf("Four of a Kind\n");
+    return 800+highest(p.hand);
+  }
 
-  if(checkT( h ) && (numOfPair(h) > 1)) // checks for full house
-    return 700+highest(h);
+  if(checkFull(p.count)){ // checks for full house
+    //printf("Full House\n");
+    return 700+highest(p.hand);
 
-  if( checkFlush(h) ) // checks for flush
-    return 600+highest(h);
+  }
 
-  if(checkStraight(h)) // checks for a straight
-    return 500+highest(h);
+  if( checkFlush(p.hand) ){ // checks for flush
+    //printf("Flush\n");
+    return 600+highest(p.hand);
 
-  if(checkT(h))
-    return 400+highest(h);
+  }
 
-  if(numOfPair( h )>1) //checks for 2 pairs
-    return 300+highest(h);
+  if(checkStraight(p.count)){ // checks for a straight
+    //printf("Straight\n");
+    return 500+highest(p.hand);
 
-  if (numOfPair( h ) == 1) { //checks for a pair
-    return 200+highest(h);
+  }
+
+  if(checkT(p.count)){
+    //printf("Triple\n");
+    return 400+highest(p.hand);
+
+  }
+
+  if(numOfPair( p.count)>1){ //checks for 2 pairs
+    //printf("2 Pairs\n");
+    return 300+highest(p.hand);
+
+  }
+
+  if (numOfPair( p.count) == 1) { //checks for a pair
+    //printf("1 Pair\n");
+    return 200+highest(p.hand);
+
 
   } else { //if the player has nothing, it returns highest card
-    return highest(h);
+    return highest(p.hand);
   }
 
 
@@ -146,21 +172,17 @@ int highest(Card h[]){
 
 //--------------------------------------------------------------------------------------------------------------
 
-//Sorting using bubble sort
-/*void sortCards(Card h[]){
-  int i;
-  int j;
-  Card * temp;
-   for (i=0; i<6; i++){
-    for (j=0; j<6-i; j++){
-      if ((*h)[j].n>(*h)[j+1].n)
-          //swap(&h[j], &h[j+1]);
-          temp=h[j];
-          h[j]=h[j+1];
-          h[j+1]=temp;
-      }
-    }
-}*/
+int comparator(void *p, void *q){
+    int x = ((Card *)p)->n;
+    int y = ((Card *)q)->n;
+    return (x - y);
+}
+
+//--------------------------------------------------------------------------------------------------------------
+//Sorting using  qsort
+void sortCards(Card h[]){
+  qsort((void*)h, 7, sizeof(h[0]), comparator);
+}
 
 //--------------------------------------------------------------------------------------------------------------
 //checks to see if all of the cards are a K Q J or T by using isRoyalty, and then checks if theyre al the same suit
@@ -182,7 +204,7 @@ bool checkRFlush(Card h[]){
 
 //checks to see if a card is a K Q J or T
 bool isRoyalty(Card a){
-  return(a.n == 10 || a.s == 11 || a.s == 12 || a.s == 13 || a.s == 14);
+  return(a.n == 10 || a.n == 11 || a.n == 12 || a.n == 13 || a.n == 14);
 }
 
 //---------------------------------------------------------------------------------------------------------------
@@ -207,10 +229,17 @@ bool checkFlush(Card h[]){
   return( (hearts>4) || (diamonds>4) || (clubs>4) || (spades>4) );
 }
 
+//------------------------------------------------------------------
+void makeCount(Player* p){
+  for(int i=0; i<7; i++){
+    p->count[p->hand[i].n-1]+=1;
+  }
+}
+
 
 //--------------------------------------------------------------------------------------------------------------
 bool checkSFlush(Card h[]){
-  bool fin; //checks for a straight and then checks to see if those cards have the same suit
+  bool fin=FALSE; //checks for a straight and then checks to see if those cards have the same suit
   int i;
   for(i=0; i<3; i++){
     if( (h[i].n == h[i+1].n) && (h[i].s == h[i+1].s) &&
@@ -227,32 +256,26 @@ bool checkSFlush(Card h[]){
 
 //--------------------------------------------------------------------------------------------------------------
 //counts number of pairs(handles 2 of a kind, adn 4 of a kind)
-int numOfPair(Card h[]){
+int numOfPair(int count[]){
   int counter=0;
-  int i, j;
-  for(i=0; i<7; i++){
-    for(j=i; j<7; j++){
-      if(i!=j){
-        if(h[i].n == h[j].n)
-          counter++;
-      }
-    }
+  int i;
+  for(i=0; i<14; i++){
+    if(count[i]==2)
+      counter++;
+    if(counter==2)
+      return 2;
   }
   return counter;
 }
 
 //--------------------------------------------------------------------------------------------------------------
 //checks for a straight
-bool checkStraight(Card h[]){
+bool checkStraight(int count[]){
   bool fin= FALSE;
   int i;
-  for(i=0; i<3; i++){
-    if( (h[i].n == h[i+1].n) &&
-        (h[i].n == h[i+2].n) &&
-        (h[i].n == h[i+3].n) &&
-        (h[i].n == h[i+4].n)
-        )
-        fin=TRUE;
+  for(i=0; i<10; i++){
+    if( count[i]>0 && count[i+1]>0 && count[i+2]>0 && count[i+3]>0 && count[i+4>0])
+        return TRUE;
   }
     return fin;
 
@@ -261,83 +284,51 @@ bool checkStraight(Card h[]){
 
 //----------------------------------------------------------------------------------------------------------------
 //checks for a full by checking for a pair and a triple
-bool checkFull(Card h[]){
+bool checkFull(int count[]){
   bool tri= FALSE; //boolean that stores whether there is a triple or not
-
-  int a;
-  int b;
-  int c;
+  bool two= FALSE;
   int i;
-
-  for(i=0; i<4; i++){
-    if((h[i].n == h[i+1].n) && (h[i].n == h[i+2].n)){ //checks for a triple and saves the cards that have been used
-      a=i;
-      b=i+1;
-      c=i+2;
+  for(i=0; i<14; i++){
+    if(count[i]==2)
+      two=TRUE;
+    else if(count[i]==3)
       tri=TRUE;
-      break;
-    }
-  }
-
-  Card re[4]; //new Array without cards used in the triple (re)
-  int counter=0;
-  int z;
-  for(z=0; z<7; z++){ //populating re
-    if(z!=a && z!=b && z!=c){
-      re[counter]=h[z];
-      counter++;
-    }
-  }
-
-  bool cp=FALSE; //cp=check for pair
-
-  //checks for a pair
-  int x;
-  int f;
-  for(x=0; x<4; x++){
-    for(f=x; f<4; f++){
-      if((re[x].n==re[f].n) && (x!=f))
-        cp=TRUE;
-        break;
-    }
   }
   //checks to see if there is a pair and a triple
-  return(cp && tri);
+  return(two && tri);
 }
 
 //----------------------------------------------------------
 //checks for three of a kind
-bool checkFour(Card h[]){
+bool checkFour(int count[]){
   bool fin= FALSE;
   int i;
-  for(i=0; i<3; i++){
-    if((h[i].n == h[i+1].n) && (h[i].n == h[i+2].n) && (h[i].n == h[i+3].n)) {
-      fin=TRUE;
-      break;
+  for(i=0; i<14; i++){
+      if(count[i]>3)
+        return TRUE;
     }
-  }
   return fin;
 }
 
 //----------------------------------------------------------
 //checks for three of a kind
-bool checkT(Card h[]){
+bool checkT(int count[]){
   bool fin= FALSE;
   int i;
-  for(i=0; i<4; i++){
-    if((h[i].n == h[i+1].n) && (h[i].n == h[i+2].n)){
-      fin=TRUE;
+  for(i=0; i<14; i++){
+      if(count[i]>2)
+        return TRUE;
     }
-  }
   return fin;
 }
+
 
 //-----------------------------------------------------------------
 
 //reads file and fills players
-void makePlayersHands(struct Player p1, struct Player p2, struct Player p3, FILE *fp){ // HELP -------------------
+void makePlayersHands(Player* p1, Player* p2, Player* p3, FILE *fp){ //  -------------------
 
-  if(fp!=NULL){
+  if(fp){
   char p11[3];
   char p12[3]; char p21[3]; char p22[3]; char p31[3]; char p32[3];
   char c1[3];
@@ -346,84 +337,87 @@ void makePlayersHands(struct Player p1, struct Player p2, struct Player p3, FILE
   char suite;
 
   fscanf(fp, "%s", p11);
-printf("%s\n", p11);
   suite= p11[1]; //segmentation fault
-printf("%s\n", suite);
-	
   num=p11[0];
 
-  p1.hand[0]=makeCard(num, suite);
+  p1->hand[0]=makeCard(num, suite);
 
   fscanf(fp, "%s", p12);
   suite= p12[1];
   num=p12[0];
 
-  p1.hand[1]=makeCard(num, suite);
+  p1->hand[1]=makeCard(num, suite);
+
 
   fscanf(fp, "%s", p21);
   suite= p21[1];
   num=p21[0];
 
-  p2.hand[0]=makeCard(num, suite);
+  p2->hand[0]=makeCard(num, suite);
 
   fscanf(fp, "%s", p22);
   suite= p22[1];
   num=p22[0];
 
-  p2.hand[1]=makeCard(num, suite);
+  p2->hand[1]=makeCard(num, suite);
 
   fscanf(fp, "%s", p31);
   suite= p31[1];
   num=p31[0];
 
-  p3.hand[0]=makeCard(num, suite);
+  p3->hand[0]=makeCard(num, suite);
 
 
   fscanf(fp, "%s", p32);
   suite= p32[1];
   num=p32[0];
 
-  p3.hand[1]=makeCard(num, suite);
+  p3->hand[1]=makeCard(num, suite);
 
   fscanf(fp, "%s", c1);
   suite= c1[1];
   num=c1[0];
 
-  p1.hand[2]=makeCard(num, suite);
-  p2.hand[2]=makeCard(num, suite);
-  p3.hand[2]=makeCard(num, suite);
+  p1->hand[2]=makeCard(num, suite);
+  p2->hand[2]=makeCard(num, suite);
+  p3->hand[2]=makeCard(num, suite);
 
   fscanf(fp, "%s", c2);
   suite= c2[1];
   num=c2[0];
 
-  p1.hand[3]=makeCard(num, suite);
-  p2.hand[3]=makeCard(num, suite);
-  p3.hand[3]=makeCard(num, suite);
+  p1->hand[3]=makeCard(num, suite);
+  p2->hand[3]=makeCard(num, suite);
+  p3->hand[3]=makeCard(num, suite);
+
 
   fscanf(fp, "%s", c3);
   suite= c3[1];
   num=c3[0];
 
-  p1.hand[4]=makeCard(num, suite);
-  p2.hand[4]=makeCard(num, suite);
-  p3.hand[4]=makeCard(num, suite);
+  p1->hand[4]=makeCard(num, suite);
+  p2->hand[4]=makeCard(num, suite);
+  p3->hand[4]=makeCard(num, suite);
+
 
   fscanf(fp, "%s", c4);
   suite= c4[1];
   num=c4[0];
 
-  p1.hand[5]=makeCard(num, suite);
-  p2.hand[5]=makeCard(num, suite);
-  p3.hand[5]=makeCard(num, suite);
+  p1->hand[5]=makeCard(num, suite);
+  p2->hand[5]=makeCard(num, suite);
+  p3->hand[5]=makeCard(num, suite);
+
 
   fscanf(fp, "%s", c5);
   suite= c5[1];
   num=c5[0];
 
-  p1.hand[6]=makeCard(num, suite);
-  p2.hand[6]=makeCard(num, suite);
-  p3.hand[6]=makeCard(num, suite);
+  p1->hand[6]=makeCard(num, suite);
+  p2->hand[6]=makeCard(num, suite);
+  p3->hand[6]=makeCard(num, suite);
+
+
 
 
 }else{
