702512fed50384a2adbb8ca5c15087ea3837c9c0
eciraco1 <43480602+eciraco1@users.noreply.github.com>
2018-10-20 20:27:01 -0400

    Update poker.h

--- poker.h
+++ poker.h
@@ -4,3 +4,21 @@ typedef int bool;
 #define TRUE 1
 #define FALSE 0
 
+int getHandValue(Player p);
+int highest(Card h[]);
+void sortCards(Card* h);
+bool checkRFlush(Card h[]);
+bool isRoyalty(Card a);
+bool checkFlush(Card h[]);
+bool checkSFlush(Card h[]);
+int numOfPair(int count[]);
+bool checkStraight(int count[]);
+bool checkFull(int count[]);
+bool checkFour(int count[]);
+bool checkT(int count[]);
+void makePlayersHands( Player* p1, Player* p2, Player* p3, FILE *fp);
+Card makeCard(char num, char suite);
+int comparator(void *p, void *q);
+void makeCount(Player* p);
+void getWinner(Player p1, Player p2, Player p3);
+
